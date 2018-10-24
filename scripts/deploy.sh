2ac5705f480dc1bcb54835a3f241ede602351430
Pascal Brandt <psbrandt@gmail.com>
2018-10-24 13:03:42 -0700

    🔓 Skip host checking.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 set -x
 
 # Clean old build
-sshpass -e ssh travis@wilcoxlab.uw.edu rm -rf /opt/wilcoxlab/www/*
+sshpass -e ssh -o StrictHostKeyChecking=no travis@wilcoxlab.uw.edu rm -rf /opt/wilcoxlab/www/*
 
 # Copy new build
-sshpass -e scp -o StrictHostKeyChecking=accept-new -r ./public/. travis@wilcoxlab.uw.edu:/opt/wilcoxlab/www/
+sshpass -e scp -o StrictHostKeyChecking=no -r ./public/. travis@wilcoxlab.uw.edu:/opt/wilcoxlab/www/
