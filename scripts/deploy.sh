261ba1fe2caa98bc8c0b90b3dec87490cf1f0dd0
Pascal Brandt <psbrandt@gmail.com>
2018-10-24 12:43:41 -0700

    🔑 Accept host key.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 set -x
 
-ls -lah
+# Clean old build
+sshpass -e ssh travis@wilcoxlab.uw.edu rm -rf /opt/wilcoxlab/www/*
 
-sshpass -e scp -r ./public/. travis@wilcoxlab.uw.edu:/opt/wilcoxlab/www/
+# Copy new build
+sshpass -e scp -o StrictHostKeyChecking=accept-new -r ./public/. travis@wilcoxlab.uw.edu:/opt/wilcoxlab/www/
